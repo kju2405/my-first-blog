41778532182d9633b730e5ec7293984d4a60d255
KimJaeUng <kju2405@naver.com>
2023-05-22 16:51:12 +0900

    Assignment 10

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 # from django.urls import path
-from django.conf.urls import url
 from django.urls import path, include
 
 urlpatterns = [
